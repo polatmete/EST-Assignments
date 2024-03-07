3b8ccc178a823b5844d929169a3e99fb4f10a0f2
maettuu <mangori41@bluewin.ch>
2024-03-07 17:20:35 +0100

    check functionality

--- assignment01/maximum_subarray/src/test/java/zest/MaximumSubarrayTest.java
+++ assignment01/maximum_subarray/src/test/java/zest/MaximumSubarrayTest.java
@@ -1,6 +1,15 @@
 package zest;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 
 class MaximumSubarrayTest {
 
+    @Test
+    void testGet() {
+        int[] a = null;
+        assertEquals("Hello JUnit 5", MaximumSubarray.maxSubArray(a));
+    }
 }
