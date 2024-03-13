b67177ebb84cc5643b3c5012136a0394fbfeea60
Jonas Gebel <jonas.gebel@bf.uzh.ch>
2024-03-13 14:21:52 +0100

    Specification-based testing done

--- assignment01/combination_sum/src/test/java/zest/CombinationSumTest.java
+++ assignment01/combination_sum/src/test/java/zest/CombinationSumTest.java
@@ -1,5 +1,117 @@
 package zest;
 
-class CombinationSumTest {
+import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class CombinationSumTest {
+    @Test
+    void nullCandidates() {
+        int[] candidates = null;
+        int target = 4;
+        List<List<Integer>> expected = Collections.emptyList();
+        List<List<Integer>> result = CombinationSum.combinationSum(candidates, target);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void emptyCandidates() {
+        int[] candidates = {};
+        int target = 4;
+        List<List<Integer>> expected = Collections.emptyList();
+        List<List<Integer>> result = CombinationSum.combinationSum(candidates, target);
+        assertEquals(expected, result);
+    }
+
+    @Test
+    void oneCandidateWithResult() {
+        int[] candidates = {2};
+        int target = 4;
+        List<List<Integer>> expected = Collections.singletonList(Arrays.asList(2, 2));
+        List<List<Integer>> result = CombinationSum.combinationSum(candidates, target);
+        assertEquals(expected, result);
+    }
+    @Test
+    void oneCandidateWithoutResult() {
+        int[] candidates = {2};
+        int target = 5;
+        List<List<Integer>> expected = Collections.emptyList();
+        List<List<Integer>> result = CombinationSum.combinationSum(candidates, target);
+        assertEquals(expected, result);
+    }
+    @Test
+    void multipleCandidates() {
+        int[] candidates = {1, 2, 3};
+        int target = 4;
+        List<List<Integer>> expected = Arrays.asList(Arrays.asList(1, 1, 1, 1), Arrays.asList(1, 1, 2), Arrays.asList(2, 2), Arrays.asList(1, 3));
+        List<List<Integer>> result = CombinationSum.combinationSum(candidates, target);
+        assertTrue(expected.size() == result.size() && expected.containsAll(result) && result.containsAll(expected));
+    }
+
+    @Test
+    void negativeCandidatesPositiveTarget() {
+        int[] candidates = {-1, -2, -3};
+        int target = 4;
+        List<List<Integer>> expected = Collections.emptyList();
+        List<List<Integer>> result = CombinationSum.combinationSum(candidates, target);
+        assertTrue(result.size() == 0 && expected.containsAll(result) && result.containsAll(expected));
+    }
+
+    @Test
+    void negativeCandidatesNegativeTarget() {
+        int[] candidates = {-1, -2, -3};
+        int target = -4;
+        List<List<Integer>> expected = Arrays.asList(Arrays.asList(-1, -1, -1, -1), Arrays.asList(-1, -1, -2), Arrays.asList(-2, -2), Arrays.asList(-1, -3));
+        List<List<Integer>> result = CombinationSum.combinationSum(candidates, target);
+        assertTrue(expected.size() == result.size() && expected.containsAll(result) && result.containsAll(expected));
+    }
+
+    @Test
+    void zeroCandidateDifferentTarget() {
+        int[] candidates = {0, 1, 2, 3};
+        int target = 4;
+        List<List<Integer>> expected = Arrays.asList(Arrays.asList(1, 1, 1, 1), Arrays.asList(1, 1, 2), Arrays.asList(2, 2), Arrays.asList(1, 3));
+        List<List<Integer>> result = CombinationSum.combinationSum(candidates, target);
+        assertTrue(expected.size() == result.size() && expected.containsAll(result) && result.containsAll(expected));
+    }
+
+    @Test
+    void zeroCandidateZeroTarget() {
+        int[] candidates = {0};
+        int target = 0;
+        List<List<Integer>> expected = Collections.emptyList();
+        List<List<Integer>> result = CombinationSum.combinationSum(candidates, target);
+        assertTrue(result.size() == 0 && expected.containsAll(result) && result.containsAll(expected));
+    }
+
+    @Test
+    void positiveCandidatesNegativeTarget() {
+        int[] candidates = {1, 2, 3};
+        int target = -4;
+        List<List<Integer>> expected = Collections.emptyList();
+        List<List<Integer>> result = CombinationSum.combinationSum(candidates, target);
+        assertTrue(result.size() == 0 && expected.containsAll(result) && result.containsAll(expected));
+    }
+
+    @Test
+    void negativeAndPositiveCandidates() {
+        int[] candidates = {-2, 1, 3};
+        int target = 3;
+        List<List<Integer>> expected = Collections.emptyList();
+        List<List<Integer>> result = CombinationSum.combinationSum(candidates, target);
+        assertTrue(result.size() == 0 && expected.containsAll(result) && result.containsAll(expected));
+    }
+
+    @Test
+    void tooManyCandidates() {
+        int[] candidates = {1, 2, 3};
+        int target = 1000;
+        List<List<Integer>> result = CombinationSum.combinationSum(candidates, target);
+        assertTrue(result.size() <= 150);
+    }
 }
