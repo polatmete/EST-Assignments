2e2004e6aa141bf8980acda7f4c79bb45db6ce56
Mete Polat <81754505+polatmete@users.noreply.github.com>
2024-03-16 17:12:03 +0100

    First specification based tests for needle_in_hay

--- assignment01/needle_in_hay/src/test/java/zest/NeedleInHayTest.java
+++ assignment01/needle_in_hay/src/test/java/zest/NeedleInHayTest.java
@@ -1,5 +1,58 @@
 package zest;
 
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class NeedleInHayTest {
+    @Test
+    void stringAtBeginning() {
+        assertEquals(0, NeedleInHay.find("needleinhaystack", "needle"));
+    }
+
+    @Test
+    void stringInMiddle() {
+        assertEquals(2, NeedleInHay.find("inneedlehaystack", "needle"));
+    }
+
+    @Test
+    void stringAtEnd() {
+        assertEquals(10, NeedleInHay.find("haystackinneedle", "needle"));
+    }
+
+    @Test
+    void stringNotFound() {
+        assertEquals(-1, NeedleInHay.find("haystack", "needle"));
+    }
+
+    @Test
+    void haystackNull() {
+        assertEquals(-1, NeedleInHay.find(null, "needle"));
+    }
+
+    @Test
+    void needleNull() {
+        assertEquals(-1, NeedleInHay.find("haystack", null));
+    }
+
+    @Test
+    void bothNull() {
+        assertEquals(-1, NeedleInHay.find(null, null));
+    }
+
+    @Test
+    void bothEmpty() {
+        assertEquals(0, NeedleInHay.find("", ""));
+    }
+
+    @Test
+    void haystackEmpty() {
+        assertEquals(-1, NeedleInHay.find("", "needle"));
+    }
+
+    @Test
+    void needleEmpty() {
+        assertEquals(-1, NeedleInHay.find("haystack", ""));
+    }
 }
