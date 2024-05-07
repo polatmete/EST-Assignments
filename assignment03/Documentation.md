1ef4bf767cea67cadec632ae29fb1330a9e84b7c
StefanieKrohmann <stefanie.krohmann@hotmail.com>
2024-05-07 17:17:31 +0200

    WIP

--- assignment03/Documentation.md
+++ assignment03/Documentation.md
@@ -1,14 +1,33 @@
 # Documentation: Effective Software Testing Lab (Assignment 3)
 
-- Deliverable: One single zip file containing all the folders that you find attached to this assignment, augmented with the
-tests you will write for each problem, as described below. Additionally, a `Documentation.md` file per exercise where
-you document your decisions, testing strategy, choices, and any assumptions made. Furthermore, create an
-Assets folder for each exercise which contains screenshots of your test results.
+- Deliverable: One single zip file containing all the folders that you find attached to this assignment, augmented with
+  the
+  tests you will write for each problem, as described below. Additionally, a `Documentation.md` file per exercise where
+  you document your decisions, testing strategy, choices, and any assumptions made. Furthermore, create an
+  Assets folder for each exercise which contains screenshots of your test results.
 - Deadline: May 20, 2024, at 18:00 (Zurich, CH, time).
 
 ## book_reviews
 
 ## bus_tracking
+A common smell is being optimistic about external resources In my test suite a concern is the reliance on external
+resources such as GPSDeviceService, which may not always be available. To
+mitigate this, I mock these external resources to ensure consistent test behavior. Alternatively, I could skip the test
+if the external resource is unavailable.
+
+I extracted the setup of the mock interfaces and classes at the beginning of the
+parent test class to minimize code duplication. The test cases x, x, x verify that the ``updateBusLocation`` method
+correctly calls the corresponding services, confirmed using Mockito's ``verify`` feature.
+
+To capture and assert specific objects passed to mocks, I utilized Mockito's argument captor feature. Specifically, I
+captured the string value passed to the ``notifyPassengers`` method of ``NotificationService``, then verified if the
+captured string meets the expected criteria.
+
+
+NOTES
+//You should stub the queries, as you now know that queries return values and do not change the object’s state; and you
+should mock commands, as you know they change the world outside the object under test.
+//TODO: testing with exact strings is not good
 
 ## cat_facts
 
--- assignment03/bus_tracking/src/test/java/zest/BusTrackingTest.java
+++ assignment03/bus_tracking/src/test/java/zest/BusTrackingTest.java
@@ -1,4 +1,81 @@
 package zest;
 
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
 public class BusTrackingTest {
+    private String busId ="bus_id";
+
+    private GPSDeviceService gpsDeviceService = mock(GPSDeviceService.class);
+    private MapService mapService = mock(MapService.class);
+    private NotificationService notificationService = mock(NotificationService.class);
+    private Location location = mock(Location.class);
+
+    private BusTracker busTracker = new BusTracker(gpsDeviceService, mapService, notificationService);
+
+
+    @Test
+    void isKeyWayPoint(){
+        when(location.isKeyWaypoint()).thenReturn(true);
+        when(gpsDeviceService.getCurrentLocation(busId)).thenReturn(location);
+
+        busTracker.updateBusLocation(busId);
+        
+        verify(mapService).updateMap(busId,location);
+
+        ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+        verify(notificationService).notifyPassengers(eq(busId), captor.capture());
+        assertEquals("The bus has arrived at " + location.getWaypointName(), captor.getValue());
+    }
+
+    @Test
+    void testMoreDetails(){
+        String waypointname = "WaypointName";
+        Double latitude = 47.3769;
+        Double longitude = 8.5417;
+
+        when(gpsDeviceService.getCurrentLocation(busId)).thenReturn(location);
+
+        when(location.getLatitude()).thenReturn(latitude);
+        when(location.getLongitude()).thenReturn(longitude);
+        when(location.getWaypointName()).thenReturn(waypointname);
+
+        busTracker.updateBusLocation(busId);
+
+        ArgumentCaptor<Location> captorLocation = ArgumentCaptor.forClass(Location.class);
+        verify(mapService).updateMap(eq(busId),captorLocation.capture());
+        assertEquals(waypointname, captorLocation.getValue().getWaypointName());
+        assertEquals(latitude, captorLocation.getValue().getLatitude());
+        assertEquals(longitude, captorLocation.getValue().getLongitude());
+
+
+    }
+
+    @Test
+    void isNotKeyWayPoint(){
+        when(location.isKeyWaypoint()).thenReturn(false);
+        when(gpsDeviceService.getCurrentLocation(busId)).thenReturn(location);
+
+        busTracker.updateBusLocation(busId);
+
+        verify(mapService).updateMap(busId,location);
+        verifyNoInteractions(notificationService);
+    }
+
+    @Test
+    void locationIsNull(){
+        location = null;
+        when(gpsDeviceService.getCurrentLocation(busId)).thenReturn(location);
+
+        busTracker.updateBusLocation(busId);
+
+        verifyNoInteractions(mapService);
+
+        ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
+        verify(notificationService).notifyPassengers(eq(busId), captor.capture());
+        assertEquals("GPS signal lost. Please check back later.", captor.getValue());
+    }
 }
