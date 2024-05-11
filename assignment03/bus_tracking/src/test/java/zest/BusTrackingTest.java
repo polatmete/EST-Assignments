6498e9bcdaf5e0196f70a50f74ff35429e12c035
StefanieKrohmann <stefanie.krohmann@hotmail.com>
2024-05-11 14:09:08 +0200

    add code review requests

--- assignment03/bus_tracking/src/test/java/zest/BusTrackingTest.java
+++ assignment03/bus_tracking/src/test/java/zest/BusTrackingTest.java
@@ -1,5 +1,6 @@
 package zest;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
 
@@ -7,68 +8,80 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 public class BusTrackingTest {
-    private String busId ="bus_id";
-    private String waypointname = "WaypointName";
-    private Double latitude = 47.3769;
-    private Double longitude = 8.5417;
-    private Location location_keyWaypoint = new Location(latitude, longitude, true, waypointname);
-    private Location location_notKeyWaypoint = new Location(latitude, longitude, false, waypointname);
+    private final String busId ="bus_id";
+    private final String waypointName = "WaypointName";
+    private final Double latitude = 47.3769;
+    private final Double longitude = 8.5417;
+    private final Location location_keyWaypoint = new Location(latitude, longitude, true, waypointName);
+    private final Location location_notKeyWaypoint = new Location(latitude, longitude, false, waypointName);
 
     // Mock Services
-    private GPSDeviceService gpsDeviceService = mock(GPSDeviceService.class);
-    private MapService mapService = mock(MapService.class);
-    private NotificationService notificationService = mock(NotificationService.class);
+    private GPSDeviceService mockGPSDeviceService;
+    private MapService mockMapService;
+    private NotificationService mockNotificationService;
+    private BusTracker busTracker;
 
+    // Argument Captors
+    private ArgumentCaptor<String> stringCaptor;
+    private ArgumentCaptor<Location> locationCaptor;
 
-    private BusTracker busTracker = new BusTracker(gpsDeviceService, mapService, notificationService);
+    @BeforeEach
+    void init(){
+        mockGPSDeviceService = mock(GPSDeviceService.class);
+        mockMapService = mock(MapService.class);
+        mockNotificationService = mock(NotificationService.class);
 
+        busTracker = new BusTracker(mockGPSDeviceService, mockMapService, mockNotificationService);
+
+        stringCaptor = ArgumentCaptor.forClass(String.class);
+        locationCaptor = ArgumentCaptor.forClass(Location.class);
+    }
 
     @Test
     void isKeyWayPoint(){
-        when(gpsDeviceService.getCurrentLocation(busId)).thenReturn(location_keyWaypoint);
+        when(mockGPSDeviceService.getCurrentLocation(busId)).thenReturn(location_keyWaypoint);
 
         busTracker.updateBusLocation(busId);
         
-        verify(mapService).updateMap(busId, location_keyWaypoint);
+        verify(mockMapService, times(1)).updateMap(busId, location_keyWaypoint);
 
-        ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
-        verify(notificationService).notifyPassengers(eq(busId), captor.capture());
-        assertEquals("The bus has arrived at " + location_keyWaypoint.getWaypointName(), captor.getValue());
+        verify(mockNotificationService, times(1)).notifyPassengers(eq(busId), stringCaptor.capture());
+        assertEquals("The bus has arrived at " + location_keyWaypoint.getWaypointName(), stringCaptor.getValue());
     }
 
     @Test
     void isNotKeyWayPoint(){
-        when(gpsDeviceService.getCurrentLocation(busId)).thenReturn(location_notKeyWaypoint);
+        when(mockGPSDeviceService.getCurrentLocation(busId)).thenReturn(location_notKeyWaypoint);
 
         busTracker.updateBusLocation(busId);
 
-        verify(mapService).updateMap(busId, location_notKeyWaypoint);
-        verifyNoInteractions(notificationService);
+        verify(mockMapService, times(1)).updateMap(busId, location_notKeyWaypoint);
+        verifyNoInteractions(mockNotificationService);
     }
 
     @Test
     void correctLocation(){
-        when(gpsDeviceService.getCurrentLocation(busId)).thenReturn(location_keyWaypoint);
+        when(mockGPSDeviceService.getCurrentLocation(busId)).thenReturn(location_keyWaypoint);
         busTracker.updateBusLocation(busId);
 
-        ArgumentCaptor<Location> captorLocation = ArgumentCaptor.forClass(Location.class);
-        verify(mapService).updateMap(eq(busId), captorLocation.capture());
 
-        assertEquals(location_keyWaypoint, captorLocation.getValue());
+        verify(mockMapService, times(1)).updateMap(eq(busId), locationCaptor.capture());
+
+        assertEquals(location_keyWaypoint, locationCaptor.getValue());
     }
 
 
     @Test
     void GPSSignalLost(){
-        location_keyWaypoint = null;
-        when(gpsDeviceService.getCurrentLocation(busId)).thenReturn(location_keyWaypoint);
+        Location location = null;
+        when(mockGPSDeviceService.getCurrentLocation(busId)).thenReturn(location);
 
         busTracker.updateBusLocation(busId);
 
-        verifyNoInteractions(mapService);
+        verifyNoInteractions(mockMapService);
 
         ArgumentCaptor<String> captor = ArgumentCaptor.forClass(String.class);
-        verify(notificationService).notifyPassengers(eq(busId), captor.capture());
+        verify(mockNotificationService, times(1)).notifyPassengers(eq(busId), captor.capture());
         assertEquals("GPS signal lost. Please check back later.", captor.getValue());
     }
 }
